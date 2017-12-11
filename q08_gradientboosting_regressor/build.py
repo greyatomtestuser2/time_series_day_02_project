09dc9cad623f84ffbcf0dd355c22b694c0be37dd
LX <alex19427@gmail.com>
2017-12-11 13:20:57 +0530

    update

--- q08_gradientboosting_regressor/build.py
+++ q08_gradientboosting_regressor/build.py
@@ -3,8 +3,8 @@ import numpy as np
 import math
 from sklearn.ensemble import GradientBoostingRegressor
 from sklearn.metrics import mean_squared_error
-from q05_feature_engineering_part4.build import q05_feature_engineering_part4
-from q02_data_splitter.build import q02_data_splitter
+from greyatomlib.time_series_day_02_project.q05_feature_engineering_part4.build import q05_feature_engineering_part4
+from greyatomlib.time_series_day_02_project.q02_data_splitter.build import q02_data_splitter
 
 fe =  ["WorkDay", "Peakhours", "Peakmonths"]
 
